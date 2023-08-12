5a0e1d7efd7faaab47a0326d05a46dc028add902
Yankı Sırtaş <yankisirtas@hotmail.com>
2023-08-12 13:19:53 +0300

    Install docker sdk

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-import subprocess
 import random
+import subprocess
+
 import requests
 
 base_images = ["debian", "ubuntu", "alpine"]
 base_image = random.choice(base_images)
+# Get all tags for randomly selected base image from docker hub API
 url = "https://hub.docker.com/v2/namespaces/library/repositories/{}/tags?page_size=100".format(
     base_image
 )
+# TODO: Iterate through all pages for all tags
 tags_response = requests.get(url).json()
 all_tags = []
 for result in tags_response["results"]:
     all_tags.append(result["name"])
 tag = random.choice(all_tags)
-print("docker build --build-arg base_image={}:{} .".format(base_image, tag))
 
 build_container = subprocess.run(
     "docker build -q --build-arg base_image={}:{} .".format(
@@ -29,6 +31,3 @@ run_container = subprocess.run(
     shell=True,
     capture_output=True,
 )
-print(run_container.stdout.decode("utf-8").strip())
-
-# build_container_id = build_container.stdout.decode("utf-8").strip()
