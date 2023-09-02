1cc5a10315503be3e53e12bcab5d02b6736f2880
Yankı Sırtaş <yankisirtas@hotmail.com>
2023-09-02 14:18:59 +0300

    Add test setup script

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import random
 import subprocess
+import time
+from subprocess import Popen
 
 import requests
 
 base_images = [
-    "debian",
-    "ubuntu",
     "alpine",
 ]  # only leave alpine here to quickly test script
 base_image = random.choice(base_images)
@@ -19,19 +19,38 @@ all_tags = []
 for result in tags_response["results"]:
     all_tags.append(result["name"])
 tag = random.choice(all_tags)
+name = "test-{}-{}".format(base_image, tag)
 print("Building image with base image: {} and tag: {}".format(base_image, tag))
 build_command = "docker build -q --build-arg base_image={}:{} .".format(base_image, tag)
 
 
 build_container = subprocess.run(build_command, shell=True, stdout=subprocess.PIPE)
 print("> " + build_command)
-print("Built image with id: {}".format(build_container.stdout.decode("utf-8").strip()))
 print("Running container...")
 
-run_command = "docker run --rm -p 80:80 -it {}".format(
-    build_container.stdout.decode("utf-8").strip()
+run_command = "docker run --rm -p 80:80 -it -d --name {} {}".format(
+    name, build_container.stdout.decode("utf-8").strip()
 )
+
 run_container = subprocess.run(
     run_command,
     shell=True,
 )
+subprocess.run(
+    "docker cp /Users/yanki/Desktop/docker-dynamic/entrypoint.sh {}:/entrypoint.sh".format(
+        name
+    ),
+    stdout=subprocess.PIPE,
+    shell=True,
+)
+# res = subprocess.run("docker exec -it {} sh".format(name), shell=True)
+# print(res)
+subprocess.run(
+    'docker exec -it {} sh -c "chmod +x ./entrypoint.sh && ./entrypoint.sh"'.format(
+        name
+    ),
+    stdout=subprocess.PIPE,
+    shell=True,
+)
+
+res = subprocess.run("docker exec -it {} sh".format(name), shell=True)
